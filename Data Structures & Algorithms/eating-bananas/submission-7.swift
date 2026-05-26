15b3690e31ef64eeb63e022bfc712b0b59d48e25
Ragul ML <ragul.lakshmanan@gmail.com>
2026-05-26 10:52:30 +0530

    Add: eating-bananas - submission-7

---
+++ Data Structures & Algorithms/eating-bananas/submission-7.swift
@@ -0,0 +1,24 @@
+class Solution {
+    func minEatingSpeed(_ piles: [Int], _ h: Int) -> Int {
+        var left = 1
+        var right = piles.max() ?? Int.max
+
+        while left < right {
+            let mid = left + (right - left) / 2
+
+            var totalCount = 0
+
+            for i in 0..<piles.count {
+                let counter = (piles[i] + mid - 1) / mid
+
+                totalCount += counter
+            }
+            if totalCount <= h {
+                    right = mid
+                } else {
+                    left = mid + 1
+                }
+        }
+        return left
+    }
+}
